c8b8a0a61f46b154df9fd1f072e53f9b79f246f6
Chanuka999 <chanukaranditha99@gmail.com>
2024-07-08 08:59:24 -0700

    new changes

--- my-app/src/components/NewsList.js
+++ my-app/src/components/NewsList.js
@@ -1,13 +1,14 @@
-import {useEffect,useState} from 'react'
+import {useEffect,useState} from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 
 
 const NewsList = (props) => {
     const {category, searchTerms} = props;
+    const {news, setNews} = useState([]);
 
     useEffect(() =>{
         const fetchNews = async () =>{
-        let url=``;
+        let url=`https://gnews.io/api/v4/top-headlines?lang=en`;
 
         if (category){
             url+=`$topic=${category}`;
@@ -19,14 +20,36 @@ const NewsList = (props) => {
 
         url +=`$tokwnt={process.env.React_APP_GNEWS_API_KEY}`;
         const responce = await fetch(url);
-       const data= await Response.json();
+       const data= await responce.json();
+       console.log(data);
+       setNews(data.article);
     };
 
         fetchNews();
     });
   return (
-    <div>NewsList</div>
+    <Container>
+      <Row>
+        {news.map((article) => (
+          <Col xs={12} md={6} lg={4} key={article.url}>
+          <card>
+            <Card.Img src={article.image} variant="top" />
+            <Card.Body>
+                <Card.Title>{article.Title}</Card.Title>
+                <Card.Text>{article.description}</Card.Text>
+                <Card.Link href={article}>Read More</Card.Link>
+            </Card.Body>
+
+            
+          </card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
+
+    
+  
 };
 
 export default NewsList
