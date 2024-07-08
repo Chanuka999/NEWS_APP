c792167903f3cb876acd07db595cf6cb2dd1516a
Chanuka999 <chanukaranditha99@gmail.com>
2024-07-08 00:45:08 -0700

    apply new changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,20 +21,48 @@ function App(){
                </Dropdown.Toggle>
                <Dropdown.Menu>
                 <Dropdown.Item>word</Dropdown.Item>
-                <Dropdown.Item>word</Dropdown.Item>
-                <Dropdown.Item>word</Dropdown.Item>
-                <Dropdown.Item>word</Dropdown.Item>
-                <Dropdown.Item>word</Dropdown.Item>
-
+                <Dropdown.Item>Business</Dropdown.Item>
+                <Dropdown.Item>Technology</Dropdown.Item>
+                <Dropdown.Item>Sports</Dropdown.Item>
+                <Dropdown.Item>Entertainment</Dropdown.Item>
+                
                </Dropdown.Menu>
              </Dropdown>
         </Nav>
 
+          <Form className="d-flex">
+            <FormControl 
+            type="text"
+             placeholder="search"
+              className="me-2" 
+              name="search" />
 
+              <Button variant="outline-primary" type="submit">
+                search
+              </Button>
+          </Form>
 
       </Navbar.Collapse>
     </Container>
    </Navbar>
+   <Container>
+    <Row>
+      <Col xs={12} md={3}>
+          <h5>Categories</h5> 
+          <Nav className="flex-column">
+            <Nav.Link>world</Nav.Link>
+            <Nav.Link>Business</Nav.Link>
+            <Nav.Link>Tecnologies</Nav.Link>
+            <Nav.Link>Sports</Nav.Link>
+            <Nav.Link>Entertainment</Nav.Link>
+            </Nav>     
+      </Col>
+
+      <Col xs={12} md={9}>
+      NEWS
+      </Col>
+    </Row>
+   </Container>
    </>
   );
 }
