a364738f4a30e6e3a9f701a2003a842c8f3ad157
Chanuka999 <chanukaranditha99@gmail.com>
2024-07-08 19:35:44 -0700

    update

--- my-app/src/components/NewsList.js
+++ my-app/src/components/NewsList.js
@@ -4,44 +4,45 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const NewsList = (props) => {
     const {category, searchTerms} = props;
-    const {news, setNews} = useState([]);
+    const [news, setNews] = useState([]);
 
     useEffect(() =>{
         const fetchNews = async () =>{
-        let url=`https://gnews.io/api/v4/top-headlines?lang=en`;
+        let url = 'https://gnews.io/api/v4/top-headlines?lang=en';
 
         if (category){
-            url+=`$topic=${category}`;
+            url+=`&topic=${category}`;
         }
         if(searchTerms){
-            url += `$q=${searchTerms}`;
+            url += `&q=${searchTerms}`;
         }
         
 
-        url +=`$tokwnt={process.env.React_APP_GNEWS_API_KEY}`;
+        url +=`&token={process.env.React_APP_GNEWS_API_KEY}`;
+        // let url = "'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + 1d1e4ed490cff6156a25e0e34f662208";
+
         const responce = await fetch(url);
        const data= await responce.json();
-       console.log(data);
-       setNews(data.article);
+       setNews(data.article );
     };
 
         fetchNews();
-    });
+    }, [searchTerms, category]);
   return (
     <Container>
       <Row>
-        {news.map((article) => (
+        {news?.map((article) => (
           <Col xs={12} md={6} lg={4} key={article.url}>
-          <card>
+          <Card>
             <Card.Img src={article.image} variant="top" />
             <Card.Body>
                 <Card.Title>{article.Title}</Card.Title>
                 <Card.Text>{article.description}</Card.Text>
-                <Card.Link href={article}>Read More</Card.Link>
+                <Card.Link href={article.url}>Read More</Card.Link>
             </Card.Body>
 
             
-          </card>
+          </Card>
           </Col>
         ))}
       </Row>
